af9f7c730acb3dfae4c47f07859c585458f4ba01
ibdems <ibrahima882001@gmail.com>
2024-12-18 20:51:57 +0000

    update

--- movie/storages.py
+++ movie/storages.py
@@ -2,5 +2,5 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class MediaStorage(S3Boto3Storage):
-    location = "media"  # Dossier "media" dans le bucket S3
+    location = "movie/media"  # Dossier "media" dans le bucket S3
     file_overwrite = False  # Empêche l'écrasement des fichiers ayant le même nom
