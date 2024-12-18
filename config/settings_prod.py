0935ebf9c0eaf3f5cc4c6579ed27e00f85633a77
ibdems <ibrahima882001@gmail.com>
2024-12-18 18:16:28 +0000

    update

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -99,9 +99,9 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 ADMINS = [("Ibrahima", "ibrahima882001@gmail.com")]
 
 # Paramètre facultatif : Configuration des e-mails (exemple avec SMTP Gmail)
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = env("EMAIL_PORT", default=587)
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = env("EMAIL_HOST", default="smtp.gmail.com")
+# EMAIL_PORT = env("EMAIL_PORT", default=587)
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
